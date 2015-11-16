68c0c55950d0591c734bc6b59c9d8fc309c6379b
JingheZ <jz4kg@virginia.edu>
2015-11-15 20:07:25 -0500

    fixed bugs

--- Preprocessing.py
+++ Preprocessing.py
@@ -4,8 +4,9 @@ Preprocess the Jester Dataset for collaborative filtering
 import random
 import numpy as np
 import pandas as pd
-import simMetrics, simInstance from Similarity_metrics
+import Similarity_metrics
 import scipy.stats
+import collections
 
 # Read the dataset
 def readData(filename, rowindex, columnindex):
@@ -40,42 +41,60 @@ def convertRating(raw):
     elif raw > 0:
         rating = 1
     else:
-        rating = -1
+        rating = 0
     return rating
 
 
-def findNeighbors(user, training, k):
+def varDistribution(train):
+    train1 = np.array(train)
+    train2 = train1.transpose()
+    train3 = list(train2)
+    p_dict = []
+    for i in range(len(train3)):
+        n1 = list(train3[i]).count(1)
+        p1 = float(n1) / len(train3[i])
+        p_dict.append({0: 1-p1, 1: p1})
+
+    return p_dict
+
+
+
+def findNeighbors(user, training, k, p_dict):
     simMatrix = []
     for i in range(len(training)):
-        sims = simInstance(simMetrics)
-        sims = sims.prepareAttribute(user, training[i])
-        sims = sims.computeSim()
-        simMatrix = [i, sims.sim1, sims.sim2, sims.sim3]
+        sims = Similarity_metrics.prepareAttribute(user, training[i], p_dict)
+        weighted_sims = Similarity_metrics.computeSim(sims)
+        weighted_sims.insert(0, training[i][0])
+        simMatrix.append(weighted_sims)
 
     simDF = pd.DataFrame(simMatrix, columns = ['index', 'overlap', 'goodall', 'eskin'])
-    simDF_overlap = simDF.sort(['overlap'], ascending = False)
+    simDF_overlap = simDF.sort_values('overlap', ascending = False)
     neighbors_overlap = simDF_overlap.iloc[0:(k+1), 0:2]
 
-    simDF_goodall = simDF.sort(['goodall'], ascending = False)
+    simDF_goodall = simDF.sort_values('goodall', ascending = False)
     neighbors_goodall = simDF_goodall.iloc[0:(k+1), [0, 2]]
 
-    simDF_eskin = simDF.sort(['eskin'], ascending = False)
+    simDF_eskin = simDF.sort_values('eskin', ascending = False)
     neighbors_eskin = simDF_eskin.iloc[0:(k+1), [0, 3]]
 
     return [neighbors_overlap, neighbors_goodall, neighbors_eskin]
 
 
-def computeRecommend(user, neighbors, test):
+def computeRecommend(user, neighbors, test, threshold):
     all_scores_bin = []
     for i in range(len(user)):
         score0 = 0
         normalization = 0
+        score = 0
         score_bin = 0
         for n in range(len(neighbors)):
-            score0 += neighbors[n][1] * test[n][i]
-            normalization += neighbors[n][1]
-        score = float(score0) / normalization
-        if score > 0:
+            neighbors1 = neighbors.values.tolist()
+            neighbor_index = int(neighbors1[n][0])
+            score0 += neighbors1[n][1] * test[neighbor_index][i]
+            normalization += neighbors1[n][1]
+        if normalization > 0:
+            score = float(score0) / normalization
+        if score > threshold:
             score_bin = 1
 
         all_scores_bin.append(score_bin)
@@ -87,7 +106,9 @@ def predictCompare(user, scores):
     FN = 0
     TN = 0
     FP = 0
-
+    precision = 0
+    recall = 0
+    F1 = 0
     for i in range(len(user)):
         if user[i] == 1 and scores[i] == 1:
             TP += 1
@@ -97,9 +118,10 @@ def predictCompare(user, scores):
             TN += 1
         else:
             FP += 1
-    precision = float(TP) / (TP + FP)
-    recall = float(TP) / (TP + FN)
-    F1 = 2 * (precision * recall) / (precision + recall)
+    if TP > 0:
+        precision = float(TP) / (TP + FP)
+        recall = float(TP) / (TP + FN)
+        F1 = 2 * (precision * recall) / (precision + recall)
     return [precision, recall, F1]
 
 
@@ -107,6 +129,7 @@ def splitTrainTestRow(data, rowindex):
     train = []
     test = []
     for i in range(len(data)):
+        data[i].insert(0, i)
         if i in rowindex:
             test.append(data[i])
         else:
@@ -114,18 +137,21 @@ def splitTrainTestRow(data, rowindex):
     return train, test
 
 
-def experiment(train, test, test_items, k):
+def experiment(train, test, test_items, p_dict, k, threshold=0.5):
     all_prediction = []
+    all_neighbors = []
     for i in range(len(test)):
-        neighbors = findNeighbors(test[i], train, k)
+        test_index = test[i][0]
+        neighbors = findNeighbors(test[i], train, k, p_dict)
         all_pred = []
         for j in range(len(neighbors)):
-            scores = computeRecommend(test[i], neighbors[j], test_items)
-            prediction = predictCompare(test[i], scores)
+            scores = computeRecommend(test_items[test_index], neighbors[j], test_items, threshold)
+            prediction = predictCompare(test_items[test_index], scores)
             all_pred.append(prediction)
         all_prediction.append(all_pred)
+        all_neighbors.append(neighbors)
 
-    return all_prediction
+    return all_prediction, all_neighbors
 
 
 def computeperformance(predictions, nmetric=3):
@@ -150,30 +176,37 @@ def resultAnalysis(results):
     avg0 = np.mean(results[0])
     avg1 = np.mean(results[1])
     avg2 = np.mean(results[2])
+    std0 = np.std(results[0])
+    std1 = np.std(results[1])
+    std2 = np.std(results[2])
     pvalue01 = scipy.stats.ttest_rel(results[0], results[1])[1]
     pvalue02 = scipy.stats.ttest_rel(results[0], results[2])[1]
     pvalue12 = scipy.stats.ttest_rel(results[1], results[2])[1]
 
-    return [avg0, avg1, avg2, pvalue01, pvalue02, pvalue12]
+    return [avg0, avg1, avg2, std0, std1, std2, pvalue01, pvalue02, pvalue12]
 
 
 if __name__ == "__main__":
 
     filename = "jester-data-2.csv"
     #randomly select 10000 patients for the experiment
-    rowindex0 = random.sample(range(0, 23500), 10000)
+    rowindex0 = random.sample(range(0, 23500), 1000)
     #randomly select 30 items used for testing
     columnindex0 = random.sample(range(1, 101), 30)
     data_traincol, data_test_col = readData(filename, rowindex0, columnindex0)
 
     #randomly select 1,000 patients for testing
-    rowindex1 = random.sample(range(0, 10000), 1000)
+    rowindex1 = random.sample(range(0, 1000), 100)
 
     training_row, testing_row = splitTrainTestRow(data_traincol, rowindex1)
+    # training_row_df = pd.DataFrame(training_row)
+    # training_row_df.describe()
 
+
+    p_dict = varDistribution(training_row)
     k = 20
-    all_predictions = experiment(training_row, testing_row, data_test_col, k)
-
+    all_predictions, all_neighbors = experiment(training_row, testing_row, data_test_col, p_dict, k, threshold=0.3)
+    'done!'
     precisions, recalls, f1s = computeperformance(all_predictions, nmetric=3)
 
     results_precision = resultAnalysis(precisions)
@@ -187,3 +220,4 @@ if __name__ == "__main__":
     results_f1 = resultAnalysis(f1s)
     print "F1 Scores:"
     print results_f1
+
--- Similarity_metrics.py
+++ Similarity_metrics.py
@@ -21,12 +21,12 @@ class simMetrics:
             self.res1 = 1
         return self.res1
 
-    def goodall1(self, x, y):
+    def goodall3(self, x, y, p):
         if x == y:
-            self.res2 = 1
+            self.res2 = 1 - p[int(x)] ** 2
         return self.res2
 
-    def eskin(self, x, y, n):
+    def eskin(self, x, y, n=2):
         """
         :param x:
         :param y:
@@ -34,36 +34,46 @@ class simMetrics:
         :return:
         """
         if x == y:
-            self.res = 1
+            self.res3 = 1
         else:
             self.res3 = float(n**2) / (n**2 + 2)
         return self.res3
 
 
-class SimInstance(object):
-    """
-    Compute the aggregated similarity between instances over all attributes
-    """
-    def __init__(self):
-        self.sim1 = 0
-        self.all_sim1 = []
-        self.sim2 = 0
-        self.all_sim2 = []
-        self.sim3 = 0
-        self.all_sim3 = []
+def prepareAttribute(data1, data2, ps):
+    all_sim1 = []
+    all_sim2 = []
+    all_sim3 = []
+    for d in range(1, len(data1)):
+        simMtric = simMetrics()
+        all_sim1.append(simMtric.overlap(data1[d], data2[d]))
+        all_sim2.append(simMtric.goodall3(data1[d], data2[d], ps[d]))
+        all_sim3.append(simMtric.eskin(data1[d], data2[d]))
+    return [all_sim1, all_sim2, all_sim3]
 
-    def prepareAttribute(self, data1, data2):
-        for d in range(len(data1)):
-            self.all_sim1.append(self.overlap(data1[d], data2[d]))
-            self.all_sim2.append(self.goodall1(data1[d], data2[d]))
-            self.all_sim3.append(self.eskin(data1[d], data2[d]))
-        return self
 
-    def computeSim(self):
-        self.sim1 = float(sum(self.all_sim1)) / len(self.all_sim1)
-        self.sim2 = float(sum(self.all_sim2)) / len(self.all_sim2)
-        self.sim3 = float(sum(self.all_sim3)) / len(self.all_sim3)
-        return self
+def computeSim(all_sims):
+    sim1 = float(sum(all_sims[0])) / len(all_sims[0])
+    sim2 = float(sum(all_sims[1])) / len(all_sims[1])
+    sim3 = float(sum(all_sims[2])) / len(all_sims[2])
+    return [sim1, sim2, sim3]
+
+if __name__ == "__main__":
+
+    user = [0, 1, -1, 0, 1]
+    train = [0, 0, 1, -1, 1]
+
+    simMtric = simMetrics()
+
+    for i in range(0,3):
+        simMtric = simMetrics()
+        print simMtric.overlap(user[i], train[i])
+        print simMtric.goodall3(user[i], train[i])
+        print simMtric.eskin(user[i], train[i])
+
+    sims = prepareAttribute(user, train)
+    weighted_sims = computeSim(sims)
+    print sims
 
 
 
